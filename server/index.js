5fcfb0e1298f8a9ce4fec51464e693c249461328
Alex Ingraham <masamune2002@gmail.com>
2023-03-12 19:51:34 -0500

    Adding server and client

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/health", (req, res) => {
+  res.send(true);
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
